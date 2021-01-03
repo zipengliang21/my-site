73143bce0676f78e2aa99a32c704565be3ddc4e6
Zipeng(Jepson) <zipengliang21@gmail.com>
2021-01-02 21:52:09 -0800

    Project Structure Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from "./views/Home";
+import NoMatch from "./views/NoMatch";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  background: #051221;
+  color: #FFFFFF;
+`;
 
 function App() {
   return (
-    <div>
-    Personal Website
-    </div>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/projects">
+            projects
+          </Route>
+          <Route path="/resume">
+            resume
+          </Route>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
---
+++ src/views/NoMatch.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NoMatch() {
+  return (
+    <div>Page Not Found</div>
+  );
+}
+
+export default NoMatch;
